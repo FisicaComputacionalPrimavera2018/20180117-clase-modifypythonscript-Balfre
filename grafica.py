554f331e0dc47429bbc340a6c1ffef742f94087b
Balfre <35463055+Balfre@users.noreply.github.com>
2018-01-17 12:19:42 -0600

    Add files via upload

---
+++ grafica.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+plt.plot([0,0,1,2,1,2,2,2,3,4,4,3,4,5,5,5,6,6,6,7,8])
+plt.ylabel('Numero de mascotas y amigos por ano')
+plt.savefig('temp.png')
+plt.show('temp.png')
